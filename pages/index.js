d110bdb7ca722b9cbd8543f077bb047316ad3121
Vinicius Pinheiro <viniropi@gmail.com>
2025-12-10 18:29:59 +0000

    modifica a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Um leitor vive mil vidas antes de morrer. O homem que nunca lê vive apenas uma.</h1>
+    return <h1>Um leitor vive mil vidas antes de morrer. O homem que nunca lê vive apenas uma. 📖📚</h1>
 }
 
 export default Home;
