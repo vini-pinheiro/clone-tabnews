5ce122e23c14bbecf4d97fd59952c7dab9f2cd0a
Vinicius Pinheiro <viniropi@gmail.com>
2026-05-05 15:26:58 +0000

    add `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,23 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    
+    const result = await client.query(queryObject);
+    
+    return result;
+  } catch (error) {
+    console.error(error);
+    
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -10,20 +27,14 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
 
-  try {
-    await client.connect();
-    const result = await client.query(queryObject);
-    return result;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
-    await client.end();
-  }
+  await client.connect();
+
+  return client;
 }
 
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
 
 function getSSLValues() {
@@ -33,5 +44,5 @@ function getSSLValues() {
     };
   }
 
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
